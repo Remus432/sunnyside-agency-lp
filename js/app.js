baa8af6bb3cd81cb68f9df126ffc4716e98a55c1
remus432 <brosdev1@gmail.com>
2021-08-05 09:20:10 +0300

    third commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 const menu = document.querySelector(".header__nav-menu")
 const hamburgerBtn = document.querySelector(".header__nav-hamburger")
+const nav = document.querySelector(".header__nav")
 
 hamburgerBtn.addEventListener("click", e => {
   menu.classList.contains("visible") ? menu.classList.remove("visible") : menu.classList.add("visible")
+})
+
+window.addEventListener("scroll", e => {
+  const scrollY = window.scrollY
+  scrollY > 100 ? nav.classList.add("sticky") : nav.classList.remove("sticky")
 })
